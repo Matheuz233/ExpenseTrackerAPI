bff9a62605d6e50650966515e08f22fb620348e4
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-09-10 17:03:39 -0300

    feat: :wrench: Criação dos Migrations e Factories

---
+++ database/factories/ExpenseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\expense>
+ */
+class ExpenseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::all()->random()->id,
+            'category' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'value' => $this->faker->numberBetween(1, 1000),
+            'date_spent' => $this->faker->date(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Expense;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(20)->create();
+        Expense::factory(20)->create();
 
         User::factory()->create([
             'name' => 'Test User',

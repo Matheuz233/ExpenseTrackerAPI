e118f9d8d42685da01b6bc08cf44f0e1a4bcc839
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-10-28 14:52:59 -0300

    feat: :wrench: Implementação da Autenticação de Usuário

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExpenseController;
@@ -9,12 +10,16 @@ use App\Http\Controllers\ExpenseController;
 // });
 
 Route::prefix("")->group(function () {
-    Route::get("/users", [UserController::class, 'index']);
-    Route::get("/users/{user}", [UserController::class, 'show']);
-    // Route::apiResource("expenses", UserController::class);
-    Route::get("/expenses", [ExpenseController::class, 'index']);
-    Route::get("/expenses/{id}", [ExpenseController::class, 'show']);
-    Route::post("/expenses", [ExpenseController::class, 'store']);
-    Route::put("/expenses/{id}", [ExpenseController::class, 'update']);
-    Route::delete("/expenses/{id}", [ExpenseController::class, 'destroy']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get("/users", [UserController::class, 'index']);
+        Route::get("/users/{user}", [UserController::class, 'show']);
+        Route::get("/expenses", [ExpenseController::class, 'index']);
+        Route::get("/expenses/{id}", [ExpenseController::class, 'show']);
+        Route::post("/expenses", [ExpenseController::class, 'store']);
+        Route::put("/expenses/{id}", [ExpenseController::class, 'update']);
+        Route::delete("/expenses/{id}", [ExpenseController::class, 'destroy']);
+    });
 });
+

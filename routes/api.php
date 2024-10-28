3e01528f67818f49090179dbdbfb74da2b128ac8
Matheus Augusto <138679799+Matheuz233@users.noreply.github.com>
2024-10-28 15:02:15 -0300

    feat: :wrench: Implementação da Autenticação de Usuário (Logout)

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::prefix("")->group(function () {
         Route::post("/expenses", [ExpenseController::class, 'store']);
         Route::put("/expenses/{id}", [ExpenseController::class, 'update']);
         Route::delete("/expenses/{id}", [ExpenseController::class, 'destroy']);
+
+        Route::post('/logout', [AuthController::class, 'logout']);
     });
 });
 
